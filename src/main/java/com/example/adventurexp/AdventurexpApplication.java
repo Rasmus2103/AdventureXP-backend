0addba18a159b5d52b69d94d966f9ece7043899f
Rasmus2103 <rasm110c@stud.kea.dk>
2023-10-02 14:05:45 +0200

    Initial commit

---
+++ src/main/java/com/example/adventurexp/AdventurexpApplication.java
@@ -0,0 +1,13 @@
+package com.example.adventurexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventurexpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventurexpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adventurexp/AdventurexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adventurexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventurexpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
