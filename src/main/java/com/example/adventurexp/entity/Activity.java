7b83549f671b1d8b57600a35481abb268b25a939
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-10-03 11:01:21 +0200

    ...

--- src/main/java/com/example/adventurexp/entity/Activity.java
+++ src/main/java/com/example/adventurexp/entity/Activity.java
@@ -1,4 +1,39 @@
 package com.example.adventurexp.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
+@Entity
 public class Activity extends AdminDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private double price;
+    @Column
+    private int minAge;
+    @Column
+    private int capacity;
+
+
+
+    public Activity(String name, double price, int minAge, int capacity) {
+        this.name = name;
+        this.price = price;
+        this.minAge = minAge;
+        this.capacity = capacity;
+    }
+
+
 }
--- src/main/java/com/example/adventurexp/entity/Reservation.java
+++ src/main/java/com/example/adventurexp/entity/Reservation.java
@@ -1,4 +1,25 @@
 package com.example.adventurexp.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.mapping.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
+@Entity
 public class Reservation extends AdminDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne()
+    private Customer customer;
+    @ManyToMany()
+    private List<Activity> activity;
+
 }
