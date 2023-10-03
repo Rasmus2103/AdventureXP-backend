557569e584d7b2c7d5bca13028aa5dbb1c1ca145
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-10-03 13:26:23 +0200

    har ændret i customer og employee så de spiller sammen med userwithroles

--- src/main/java/com/example/adventurexp/adventure/repository/EmployeeRepo.java
+++ src/main/java/com/example/adventurexp/adventure/repository/EmployeeRepo.java
@@ -3,5 +3,5 @@ package com.example.adventurexp.adventure.repository;
 import com.example.adventurexp.adventure.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
+public interface EmployeeRepo extends JpaRepository<Employee,String> {
 }
