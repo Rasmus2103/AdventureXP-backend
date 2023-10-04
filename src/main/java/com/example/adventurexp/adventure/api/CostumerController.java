f836c7aee1e0a55789ec16d0f92f1a392ce11120
Rasmus2103 <rasm110c@stud.kea.dk>
2023-10-04 12:14:49 +0200

    commit

--- src/main/java/com/example/adventurexp/adventure/api/CostumerController.java
+++ src/main/java/com/example/adventurexp/adventure/api/CostumerController.java
@@ -1,11 +1,50 @@
 package com.example.adventurexp.adventure.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.adventurexp.adventure.dto.CustomerRequest;
+import com.example.adventurexp.adventure.dto.CustomerResponse;
+import com.example.adventurexp.adventure.dto.EmployeeRequest;
+import com.example.adventurexp.adventure.dto.EmployeeResponse;
+import com.example.adventurexp.adventure.entity.Customer;
+import com.example.adventurexp.adventure.service.CustomerService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("api/costumer")
 public class CostumerController {
+
+    CustomerService customerService;
+
+    public CostumerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    List<CustomerResponse> getCustomers() {
+        return customerService.getCustomers(false);
+    }
+
+    @GetMapping(path = "/{username")
+    CustomerResponse getCustomerByUsername(@PathVariable String username) {
+        return customerService.findById(username);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    CustomerResponse addCustomer(@RequestBody CustomerRequest body) {
+        return customerService.addCustomer(body);
+    }
+
+    @PutMapping(path = "/{username}")
+    ResponseEntity<Boolean> editCustomer(@RequestBody CustomerRequest body, @PathVariable String username) {
+        return customerService.editCustomer(body, username);
+    }
+
+    @DeleteMapping(path="/{username}")
+    void deleteCustomer(@PathVariable String username) {
+        customerService.deleteCustomer(username);
+    }
 }
--- src/main/java/com/example/adventurexp/adventure/service/CustomerService.java
+++ src/main/java/com/example/adventurexp/adventure/service/CustomerService.java
@@ -1,7 +1,17 @@
 package com.example.adventurexp.adventure.service;
 
+import com.example.adventurexp.adventure.dto.*;
+import com.example.adventurexp.adventure.entity.Activity;
+import com.example.adventurexp.adventure.entity.Customer;
+import com.example.adventurexp.adventure.entity.Employee;
 import com.example.adventurexp.adventure.repository.CustomerRepo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerService {
@@ -10,6 +20,55 @@ public class CustomerService {
 
     public CustomerService(CustomerRepo customerRepo) {
         this.customerRepo = customerRepo;
+    }
+
+    public List<CustomerResponse> getCustomers(boolean includeAll) {
+        List<Customer> customers = customerRepo.findAll();
+
+        return customers.stream()
+                .map(customer -> new CustomerResponse(customer, includeAll))
+                .collect(Collectors.toList());
+    }
+
+    public CustomerResponse addCustomer(CustomerRequest body) {
+        if(customerRepo.existsById(body.getUsername())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"This employee already exists");
+        }
+
+        Customer newCustomer = CustomerRequest.getCustomerEntity(body);
+
+        newCustomer = customerRepo.save(newCustomer);
+        return new CustomerResponse(newCustomer, true);
+    }
+
+    public ResponseEntity<Boolean> editCustomer(CustomerRequest body, String username) {
+        Customer customer = getCustomerByUsername(username);
+        if(!body.getUsername().equals(username)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Cannot change username");
+        }
+        customer.setPassword(body.getPassword());
+        customer.setEmail(body.getEmail());
+        customer.setFirstName(body.getFirstName());
+        customer.setLastName(body.getLastName());
+        customer.setAddress(body.getAddress());
+        customer.setPhoneNumber(body.getPhoneNumber());
+        customerRepo.save(customer);
+        return ResponseEntity.ok(true);
+    }
+
+    public void deleteCustomer(String username) {
+        Customer customer = getCustomerByUsername(username);
+        customerRepo.delete(customer);
+    }
+
+    public CustomerResponse findById(String username) {
+        Customer customer = getCustomerByUsername(username);
+        return new CustomerResponse(customer, true);
+    }
+
+    private Customer getCustomerByUsername(String username) {
+        return customerRepo.findById(username).
+                orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Member with this username does not exist"));
     }
 
 }
