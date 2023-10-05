3c00362e3005e19652ca68c3e776cb3e175b956c
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-10-05 12:54:06 +0200

    har lavet test i shiftServiceTest

--- src/test/java/com/example/adventurexp/service/shiftServiceTest.java
+++ src/test/java/com/example/adventurexp/service/shiftServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.adventurexp.service;
 
+import com.example.adventurexp.adventure.dto.*;
 import com.example.adventurexp.adventure.entity.Employee;
 import com.example.adventurexp.adventure.entity.Shift;
 import com.example.adventurexp.adventure.repository.EmployeeRepo;
@@ -7,11 +8,18 @@ import com.example.adventurexp.adventure.repository.ShiftRepo;
 import com.example.adventurexp.adventure.service.EmployeeService;
 import com.example.adventurexp.adventure.service.ShiftService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 public class shiftServiceTest {
@@ -35,6 +43,47 @@ public class shiftServiceTest {
         s2 = shiftRepo.save(new Shift(e2, LocalDateTime.now(), LocalDateTime.now().plusDays(3)));
         shiftService = new ShiftService(shiftRepo, employeeRepo);
 
+    }
+
+    @Test
+    void getShifts() {
+        List<ShiftResponse> shiftResponse = shiftService.getShifts(true);
+        EmployeeResponse employeeResponse = shiftResponse.get(0).getEmployeeResponse();
+        LocalDateTime startDate = shiftResponse.get(0).getShiftStart();
+        LocalDateTime endDate = shiftResponse.get(0).getShiftEnd();
+        assertNotNull(employeeResponse);
+        assertNotNull(startDate);
+        assertNotNull(endDate);
+    }
+
+    /*
+    @Test
+    void testFindByIdFound(){
+        ReservationResponse response = shiftService.findById(s1.getId());
+        assertEquals(s1.getId(), response.getId());
+    }
+
+    @Test
+    void findByIdNotFound() {
+        ResponseStatusException ex = assertThrows(ResponseStatusException.class, () -> shiftService.findById(3));
+        assertEquals(HttpStatus.NOT_FOUND, ex.getStatusCode());
+    }
+    */
+
+    @Test
+    void addShiftSucces() {
+        ShiftRequest s3 = new ShiftRequest(e1.getUsername(), LocalDateTime.now().plusDays(2), LocalDateTime.now().plusDays(5));
+        ShiftResponse response = shiftService.addShift(s3, true);
+        assertEquals(e1.getUsername(), response.getEmployeeResponse().getUsername());
+
+    }
+
+
+    @Test
+    void addShiftStartDateBeforeToday() {
+        ShiftRequest s3 = new ShiftRequest(e1.getUsername(), LocalDateTime.now().minusDays(2), LocalDateTime.now().plusDays(5));
+        ResponseStatusException ex = assertThrows(ResponseStatusException.class, () -> shiftService.addShift(s3, true));
+        assertEquals(HttpStatus.BAD_REQUEST, ex.getStatusCode());
     }
 
 
