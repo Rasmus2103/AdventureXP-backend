18becb0bb017ff621f6300d156cbd9058fa08b42
Rasmus2103 <rasm110c@stud.kea.dk>
2023-10-08 16:12:12 +0200

    Lavet lidt

--- src/main/java/com/example/adventurexp/security/config/SecurityConfig.java
+++ src/main/java/com/example/adventurexp/security/config/SecurityConfig.java
@@ -90,7 +90,8 @@ public class SecurityConfig {
             //Shift Endpoints
                     .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET, "api/shift")).permitAll()
                     .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.POST, "api/shift")).permitAll()
-                    .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.PUT, "api/shift{id}")).permitAll()
+                    .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.PUT, "api/shift/{id}")).permitAll()
+                    .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.DELETE, "api/shift/{id}")).permitAll()
 
             //Customer Endpoints
                     .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET, "/api/costumer")).permitAll()
